7e8d94a123548d7b5f9039f301d73a6c8542dbe0
sopharsogood <justanothersophie@gmail.com>
2021-07-21 12:08:36 -0400

    commit tree 88f4c17ba217248a072da003599ad1b8e70ea3b9

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(uncertain_prime)
+    return false if uncertain_prime < 2 or !(uncertain_prime.is_a? Integer)
+    sieve = (2..uncertain_prime).to_a
+    loop do
+        if uncertain_prime % sieve.first == 0
+            return true if uncertain_prime == sieve.first
+            return false
+        else
+            filter = sieve.first
+            sieve.delete_if {|filtered_number| filtered_number % filter == 0}
+        end
+    end
+end
+
+# could have just used .any? and it would be simpler
